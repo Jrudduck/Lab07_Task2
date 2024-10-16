871ca49e3620b4c12aa200726709ac822ad23a64
614788 <rudduck.614788@swl.k12.oh.us>
2024-10-16 08:43:52 -0400

    Initial commit

---
+++ Multiply/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+     for (int i = 0; i <= 4; i++){
+            for (int r = 0; r <= i; r++) {
+                 System.out.print("*");
+            }
+            System.out.println();
+     }
+        System.out.println();
+
+     for (int i = 5; i >= 0; i--) {
+         for (int r = 1; r <= i; r++) {
+             System.out.print("*");
+         }
+         System.out.println();
+     }
+     
+
+     for (int i = 0; i <= 4; i++){
+             for (int r = 0; r <= 4; r++) {
+                 System.out.print("*");
+             }
+             System.out.println();
+     }
+
+
+    }
+}
